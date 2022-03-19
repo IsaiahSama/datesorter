df51063036bda7dce9788c8f5e56222d4a1bc501
Isaiah <isaiahcarrington46@gmail.com>
2022-03-19 08:16:06 -0400

    Added input validation

--- mechanics.py
+++ mechanics.py
@@ -52,3 +52,33 @@ class Database:
         with connect(DBNAME) as db:
             db.execute("INSERT INTO DATETABLE (DATE, EVENT) VALUES (?, ?)", (date, event))
             db.commit()
+
+class InputValidation:
+    """Class used to handle input validation.
+    
+    Attrs:
+        None
+        
+    Methods:
+        validateInt(prompt:str): Used to ensure that the user enters a 4 digit integer"""
+
+    def __init__(self):
+        pass 
+
+    def validateInt(self, prompt:str) -> int:
+        """Method that ensures the user enters a 4 digit integer.
+        
+        Args:
+            prompt (str): The prompt to display the user
+            
+        Returns:
+            int"""
+            
+        valid = 0
+        while not valid:
+            result = input(prompt + ": ")
+            if not result.isnumeric(): print("Your response must be numeric, not", result)
+            elif len(result != 4): print("Your response must be no more or less than 4 digits, not", len(result), "digits")
+            else: valid = 1
+
+        return int(result)
